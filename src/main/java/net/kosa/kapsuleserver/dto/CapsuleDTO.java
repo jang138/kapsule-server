20a8e06a154fec9b7c1bb9e123fff60d48d3e40f
이다영 <yooung513@gmail.com>
2024-08-22 23:48:26 +0900

    Update CapsuleDTO.java

--- src/main/java/net/kosa/kapsuleserver/dto/CapsuleDTO.java
+++ src/main/java/net/kosa/kapsuleserver/dto/CapsuleDTO.java
@@ -1,4 +1,24 @@
 package net.kosa.kapsuleserver.dto;
 
+import java.time.LocalDate;
+
+import net.kosa.kapsuleserver.entity.Member;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class CapsuleDTO {
+
+	private Long id;
+	private Member member;
+	private String title;
+	private String content;
+	private String address;
+	private Float longitude;
+	private Float latitude;
+	private LocalDate unlockDate;
+	private String capsuleCode;
+
 }
