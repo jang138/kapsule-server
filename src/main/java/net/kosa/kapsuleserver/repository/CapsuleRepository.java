ade508bfff214a287f6c7b1881d1201a3ae64c7a
이다영 <yooung513@gmail.com>
2024-08-23 15:23:40 +0900

    Update Repo 주석 변경 (제거)

--- src/main/java/net/kosa/kapsuleserver/repository/CapsuleRepository.java
+++ src/main/java/net/kosa/kapsuleserver/repository/CapsuleRepository.java
@@ -3,10 +3,6 @@ package net.kosa.kapsuleserver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import net.kosa.kapsuleserver.entity.Capsule;
 
-/**
- * @author dayoung
- * CapsuleRepository는 타임캡슐과 관련된 데이터를 DB에서 가져옵니다.
- */
 public interface CapsuleRepository extends JpaRepository<Capsule, Long> {
 	boolean existsByCapsuleCode(String capsuleCode);
 }
