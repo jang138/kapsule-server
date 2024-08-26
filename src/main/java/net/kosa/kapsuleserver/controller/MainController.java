d0ba9bfbb105e5c642d1c1cb29b8acade2fbb16c
onestone <onestone1380@gmail.com>
2024-08-26 10:56:40 +0900

    Feat Security & JWT

---
+++ src/main/java/net/kosa/kapsuleserver/controller/MainController.java
@@ -0,0 +1,15 @@
+package net.kosa.kapsuleserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+
+        return "main";
+    }
+}
