6403b98ecd79894f724a460755c0663966434ca2
이다영 <yooung513@gmail.com>
2024-08-27 10:36:58 +0900

    Update CapsuleController.java

--- src/main/java/net/kosa/kapsuleserver/controller/CapsuleController.java
+++ src/main/java/net/kosa/kapsuleserver/controller/CapsuleController.java
@@ -5,10 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import net.kosa.kapsuleserver.base.util.LoginUtil;
 import net.kosa.kapsuleserver.dto.CapsuleDTO;
